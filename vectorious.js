a891c382e0e6c51939585a6cca9a154d7bdff7ff
Bart van Andel <bavanandel@gmail.com>
2017-01-26 22:53:30 +0100

    Supply explicit 'withblas' and 'withoutblas' entry points

--- vectorious.js
+++ vectorious.js
@@ -1,82 +1,15 @@
 (function () {
   'use strict';
 
-  var Vector = require('./vector'),
-      Matrix = require('./matrix');
+  var Vector = module.exports.Vector = require('./vector'),
+      Matrix = module.exports.Matrix = require('./matrix');
+
   try {
-    var nblas = require('nblas');
+    var nblas = module.exports.BLAS = require('nblas'),
+        applyBlasOptimizations = require('./applyBlasOptimizations');
+    
+    applyBlasOptimizations(Vector, Matrix, nblas);
   } catch (error) {
-    module.exports.Vector = Vector;
-    module.exports.Matrix = Matrix;
-    return;
+
   }
-
-  // BLAS optimizations
-  Vector.prototype.add =
-  Matrix.prototype.add = function (data) {
-    var l1 = this instanceof Vector ? this.length : this.shape[0] * this.shape[1],
-        l2 = data instanceof Vector ? data.length : data.shape[0] * data.shape[1];
-    if (l1 !== l2)
-      throw new Error('sizes do not match!');
-    if (!l1 && !l2)
-      return this;
-
-    nblas.axpy(data.data, this.data);
-    return this;
-  };
-
-  Vector.prototype.subtract =
-  Matrix.prototype.subtract = function (data) {
-    var l1 = this instanceof Vector ? this.length : this.shape[0] * this.shape[1],
-        l2 = data instanceof Vector ? data.length : data.shape[0] * data.shape[1];
-    if (l1 !== l2)
-      throw new Error('sizes do not match!');
-    if (!l1 && !l2)
-      return this;
-
-    nblas.axpy(data.data, this.data, -1);
-    return this;
-  };
-
-  Vector.prototype.scale =
-  Matrix.prototype.scale = function (scalar) {
-    nblas.scal(this.data, scalar);
-    return this;
-  };
-
-  Vector.prototype.dot = function (vector) {
-    if (this.length !== vector.length)
-      throw new Error('sizes do not match!');
-
-    return nblas.dot(this.data, vector.data);
-  };
-
-  Vector.prototype.magnitude = function () {
-    if (!this.length)
-      return 0;
-
-    return nblas.nrm2(this.data);
-  };
-
-  Vector.prototype.max = function() {
-    return this.data[nblas.idamax(this.length, this.data, 1)];
-  };
-
-  Matrix.prototype.multiply = function(matrix) {
-    var r1 = this.shape[0],
-        c1 = this.shape[1],
-        r2 = matrix.shape[0],
-        c2 = matrix.shape[1],
-        data = new this.type(r1 * c2);
-
-    if (c1 !== r2)
-      throw new Error('sizes do not match');
-
-    nblas.gemm(this.data, matrix.data, data, r1, c2, c1);
-    return Matrix.fromTypedArray(data, [r1, c2]);
-  };
-
-  module.exports.Vector = Vector;
-  module.exports.Matrix = Matrix;
-  module.exports.BLAS = nblas;
 }());
