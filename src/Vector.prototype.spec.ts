384ad009cf6fa0a2e528faae0e2bd6e016a8968e
Mateo Gianolio <gianoliomateo@gmail.com>
2019-02-10 23:59:30 +0100

    split up spec files in two

---
+++ src/Vector.prototype.spec.ts
@@ -0,0 +1,299 @@
+import { Vector } from './';
+import * as assert from 'assert';
+
+describe('Vector.prototype', () => {
+  describe('.add()', () => {
+    it('should return empty vector if adding two empty vectors', () => {
+      var a = new Vector();
+      var b = new Vector();
+
+      assert.deepEqual(new Vector(), a.add(b));
+    });
+
+    it('should throw error if sizes do not match', () => {
+      var a = new Vector([1]);
+      var b = new Vector([1, 2]);
+
+      assert.throws(a.add.bind(a, b), Error);
+    });
+
+    it('should produce Vector(5, 7, 9) from Vector(1, 2, 3) and Vector(4, 5, 6)', () => {
+      var a = new Vector([1, 2, 3]);
+      var b = new Vector([4, 5, 6]);
+      var c = new Vector([5, 7, 9]);
+
+      assert.deepEqual(c, a.add(b));
+    });
+  });
+
+  describe('.subtract()', () => {
+    it('should return empty vector if subtracting two empty vectors', () => {
+      var a = new Vector();
+      var b = new Vector();
+
+      assert.deepEqual(new Vector(), a.subtract(b));
+    });
+
+    it('should throw error if sizes do not match', () => {
+      var a = new Vector([1]);
+      var b = new Vector([1, 2]);
+
+      assert.throws(a.subtract.bind(a, b), Error);
+    });
+
+    it('should produce Vector(-3, -3, -3) from Vector(1, 2, 3) and Vector(4, 5, 6)', () => {
+      var a = new Vector([1, 2, 3]);
+      var b = new Vector([4, 5, 6]);
+      var c = new Vector([-3, -3, -3]);
+
+      assert.deepEqual(c, a.subtract(b));
+    });
+  });
+
+  describe('.scale()', () => {
+    it('should scale Vector(1, 2, 3) by 2 to Vector(2, 4, 6)', () => {
+      var a = new Vector([1, 2, 3]);
+      var b = new Vector([2, 4, 6]);
+
+      assert.deepEqual(b, a.scale(2));
+    });
+  });
+
+  describe('.normalize()', () => {
+    it('should work as expected', () => {
+      var a = new Vector([1, 1]);
+      var b = new Vector([1 / Math.sqrt(2), 1 / Math.sqrt(2)]);
+
+      assert.deepEqual(b, a.normalize());
+    });
+  });
+
+  describe('.project()', () => {
+    it('should throw error if sizes do not match', () => {
+      var a = new Vector([1]);
+      var b = new Vector([1, 2]);
+
+      assert.throws(a.project.bind(a, b), Error);
+    });
+
+    it('should work as expected', () => {
+      var a = new Vector([2, 1]);
+      var b = new Vector([-3, 4]);
+      var c = new Vector([6 / 25, -8 / 25]);
+
+      assert.deepEqual(c, a.project(b));
+    });
+  });
+
+  describe('.range()', () => {
+    it('should throw error if wrong number or arguments supplied', () => {
+      assert.throws(Vector.range.bind(new Vector(), 1), Error);
+      assert.throws(Vector.range.bind(new Vector(), 1, 2, 3, 4), Error);
+    });
+
+    it('should throw error if step > start - end', () => {
+      assert.throws(Vector.range.bind(new Vector(), 0, 0), Error);
+      assert.throws(Vector.range.bind(new Vector(), 1, 3, 2), Error);
+    });
+
+    it('should work as expected', () => {
+      var a = Vector.range(0, 5);
+      var b = Vector.range(5, 2, 10);
+      var c = Vector.range(5, 0);
+      var d = Vector.range(5, 2, 0);
+
+      assert.deepEqual(new Vector([0, 1, 2, 3, 4]), a);
+      assert.deepEqual(new Vector([5, 7, 9]), b);
+      assert.deepEqual(new Vector([5, 4, 3, 2, 1]), c);
+      assert.deepEqual(new Vector([5, 3, 1]), d);
+    });
+  });
+
+  describe('.dot()', () => {
+    it('should throw error if sizes do not match', () => {
+      var a = new Vector([1]);
+      var b = new Vector([1, 2]);
+
+      assert.throws(a.dot.bind(a, b), Error);
+    });
+
+    it('should work as expected', () => {
+      var a = new Vector([1, 2, 3]);
+      var b = new Vector([4, 5, 6]);
+
+      assert.equal(32, a.dot(b));
+    });
+  });
+
+  describe('.magnitude()', () => {
+    it('should return 0 if empty vector', () => {
+      assert.equal(0, new Vector().magnitude());
+    });
+
+    it('should work as expected', () => {
+      assert.equal(4, new Vector([1, 1, 1, 2, 3]).magnitude());
+    });
+  });
+
+  describe('.angle()', () => {
+    it('should work as expected', () => {
+      var a = new Vector([1, 0]);
+      var b = new Vector([0, 1]);
+
+      assert.equal(Math.PI / 2, a.angle(b));
+    });
+  });
+
+  describe('.equals()', () => {
+    it('should work as expected', () => {
+      assert.equal(true, new Vector([1, 3, 2]).equals(new Vector([1, 3, 2])));
+      assert.equal(true, new Vector().equals(new Vector()));
+      assert.equal(false, new Vector([1, 2, 3]).equals(new Vector([1, 3, 2])));
+    });
+  });
+
+  describe('.get()', () => {
+    it('should throw error if index out of bounds', () => {
+      var a = new Vector([1, 2, 3]);
+      assert.throws(a.get.bind(a, -1), Error);
+      assert.throws(a.get.bind(a, 3), Error);
+    });
+
+    it('should work as expected', () => {
+      var a = new Vector([1, 3, 2, 4]);
+      assert.equal(1, a.get(0));
+      assert.equal(3, a.get(1));
+      assert.equal(2, a.get(2));
+      assert.equal(4, a.get(3));
+    });
+  });
+
+  describe('.x, .y, .z, .w', () => {
+    it('should retrieve properties as expected', () => {
+      var a = new Vector([1,2,3,4,5]);
+
+      assert.equal(a.x, 1)
+      assert.equal(a.y, 2)
+      assert.equal(a.z, 3)
+      assert.equal(a.w, 4)
+    });
+
+    it('should set proeprties as expected', () => {
+      var a = new Vector([-1,-1,-1,-1]);
+
+      a.x = 0;
+      a.y = 1;
+      a.z = 2;
+      a.w = 3;
+
+      assert.equal(a.get(0), 0);
+      assert.equal(a.get(1), 1);
+      assert.equal(a.get(2), 2);
+      assert.equal(a.get(3), 3);
+
+    })
+  })
+
+  describe('.min()', () => {
+    it('should find the minimum number in vectors', () => {
+      var a = new Vector([1, 2, 3]);
+      var b = new Vector([3, -1, 1]);
+      var c = new Vector([2, 5, 1]);
+
+      assert.equal(1, a.min());
+      assert.equal(-1, b.min());
+      assert.equal(1, c.min());
+    });
+  });
+
+  describe('.max()', () => {
+    it('should find the maximum number in vectors', () => {
+      var a = new Vector([1, 2, 3]);
+      var b = new Vector([3, -1, 1]);
+      var c = new Vector([2, 5, 1]);
+
+      assert.equal(3, a.max());
+      assert.equal(3, b.max());
+      assert.equal(5, c.max());
+    });
+  });
+
+  describe('.set()', () => {
+    it('should throw error if index out of bounds', () => {
+      var a = new Vector([1, 2]);
+      assert.throws(a.set.bind(a, -1, 0), Error);
+      assert.throws(a.set.bind(a, 2, 0), Error);
+    });
+
+    it('should work as expected', () => {
+      var a = new Vector([1, 2]);
+      a.set(0, 0);
+      a.set(1, 1);
+      assert.equal(0, a.get(0));
+      assert.equal(1, a.get(1));
+    });
+  });
+
+  describe('.combine()', () => {
+    it('should return current vector if combined with empty vector', () => {
+      assert.deepEqual(new Vector([1, 2, 3]), new Vector([1, 2, 3]).combine(new Vector()));
+    });
+
+    it('should work as expected', () => {
+      assert.deepEqual(new Vector([1, 2, 3, 0, 1]), new Vector([1, 2, 3]).combine(new Vector([0, 1])));
+    });
+  });
+
+  describe('.push()', () => {
+    it('should start with Vector(1, 2), push(3) to get Vector(1, 2, 3)', () => {
+      assert.deepEqual(new Vector([1, 2, 3]), new Vector([1, 2]).push(3));
+    });
+  });
+
+  describe('.map()', () => {
+    it('should work as expected', () => {
+      var a = new Vector([1, 2, 3]);
+      var b = a.map(value => value * value);
+
+      assert.deepEqual(new Vector([1, 4, 9]), b);
+    });
+  });
+
+  describe('.each()', () => {
+    it('should work as expected', () => {
+      var a = new Vector([1, 2, 3]);
+      var b = new Vector();
+      a.each((value, index) => {
+        b.push(value * index);
+      });
+
+      assert.deepEqual(new Vector([0, 2, 6]), b);
+    });
+  });
+
+  describe('.reduce()', () => {
+    it('should work as expected', () => {
+      function sum(a: number, b: number) {
+        return a + b;
+      }
+
+      var a = new Vector([1, 2, 3]);
+      var b = new Vector([1, 2, 3, 4, 5, 6]);
+
+      assert.deepEqual(6, a.reduce(sum));
+      assert.deepEqual(21, b.reduce(sum));
+    });
+  });
+
+  describe('.toString()', () => {
+    it('should work as expected', () => {
+      assert.equal('[1, 2, 3]', new Vector([1, 2, 3]).toString());
+    });
+  });
+
+  describe('.toArray()', () => {
+    it('should work as expected', () => {
+      assert.deepEqual([1, 2, 3], new Vector([1, 2, 3]).toArray());
+    });
+  });
+});
