d6a9d81613d3b4d7608a927d0c43a11df46fbd18
Mateo Gianolio <gianoliomateo@gmail.com>
2019-08-05 16:11:12 +0200

    cleanup jacobi method

--- src/Matrix/jacobi.ts
+++ src/Matrix/jacobi.ts
@@ -1,94 +1,89 @@
 import { Matrix } from './';
 
-const maxElem:
-  <T extends Matrix>(x: T) => number[] =
-  <T extends Matrix>(x: T): [number, number, number] => {
-  const [n] = x.shape;
-
-  let max: number = 0;
-  let k: number = 0;
-  let l: number = 0;
-
-  let i: number;
-  let j: number;
-  for (i = 0; i < n; i += 1) {
-    for (j = i + 1; j < n; j += 1) {
-      if (Math.abs(x.get(i, j)) >= max) {
-        max = Math.abs(x.get(i, j));
-        k = i;
-        l = j;
-      }
-    }
-  }
-
-  return [max, k, l];
-};
-
+/**
+ *  ┌   ┐    ┌     ┐┌   ┐
+ *  │Skl│    │c  −s││Skl│
+ *  │   │ := │     ││   │
+ *  │Sij│    │s   c││Sij│
+ *  └   ┘    └     ┘└   ┘
+ */
 const rotate:
-  <T extends Matrix>(x: T, p: T, i: number, j: number) => void =
-  <T extends Matrix>(x: T, p: T, i: number, j: number): void => {
-  const [n] = x.shape;
-  const xDiff: number = x.get(j, j);
+  <T extends Matrix>(x: T, c: number, s: number, k: number, l: number, i: number, j: number) => void =
+  <T extends Matrix>(x: T, c: number, s: number, k: number, l: number, i: number, j: number): void => {
+    const temp: number = x.get(k, l);
+    const tau: number = 1 / (c + s);
 
-  let t: number;
-  if (Math.abs(x.get(i, j)) < Math.abs(xDiff) * 1e-36) {
-    t = x.get(i, j) / xDiff;
-  } else {
-    const phi: number = xDiff / 2 * x.get(i, j);
-    t = 1 / (Math.abs(phi) + Math.sqrt(phi * phi + 1));
-  }
+    x.set(k, l, temp - s * (x.get(i, j) + tau * temp));
+    x.set(i, j, x.get(i, j) + s * (temp - tau * x.get(i, j)));
+  };
 
-  const c: number = 1 / Math.sqrt(t * t + 1);
-  const s: number = t * c;
-  const tau: number = s / (c + 1);
-
-  let temp: number = x.get(i, j);
-  x.set(i, j, 0);
-  x.set(i, i, x.get(i, i) - t * temp);
-  x.set(j, j, x.get(j, j) + t * temp);
-
-  let k: number;
-  for (k = 0; k < i; k += 1) {
-    temp = x.get(k, i);
-    x.set(k, i, temp - s * (x.get(k, j) + tau * temp));
-    x.set(k, j, x.get(k, j) + s * (temp - tau * x.get(k, j)));
-  }
-
-  for (k = i + 1; k < j; k += 1) {
-    temp = x.get(i, k);
-    x.set(i, k, temp - s * (x.get(k, j) + tau * x.get(i, k)));
-    x.set(k, j, x.get(k, j) + s * (temp - tau * x.get(k, j)));
-  }
-
-  for (k = j + 1; k < n; k += 1) {
-    temp = x.get(i, k);
-    x.set(i, k, temp - s * (x.get(j, k) + tau * temp));
-    x.set(j, k, x.get(j, k) + s * (temp - tau * x.get(j, k)));
-  }
-
-  for (k = 0; k < n; k += 1) {
-    temp = p.get(k, i);
-    p.set(k, i, temp - s * (p.get(k, j) + tau * p.get(k, i)));
-    p.set(k, j, p.get(k, j) + s * (temp - tau * p.get(k, j)));
-  }
-};
-
+/**
+ * Gets eigenvalues and eigenvectors of `x` using the Jacobi method
+ */
 Matrix.jacobi = <T extends Matrix>(x: T): [T, T] => x.copy().jacobi();
 
+/**
+ * Gets eigenvalues and eigenvectors of the current matrix using the Jacobi method
+ */
 Matrix.prototype.jacobi = function<T extends Matrix>(this: T): [T, T] {
   this.square();
 
   const [n] = this.shape;
   const p: T = Matrix.eye(n);
 
-  let i: number;
-  for (i = 0; i < (n * n) * 5; i += 1) {
-    const [max, j, k] = maxElem(this);
-    if (max < 1e-9) {
-      return [this.diagonal(), p];
-    }
-    rotate(this, p, j, k);
-  }
+  let max: number = 0;
+  let i: number = 0;
+  let j: number = 0;
+  let k: number = 0;
+  let l: number = 0;
 
-  throw new Error('jacobi did not converge');
+  do {
+    // Find maximum off-diagonal element
+    for (i = 0; i < n; i += 1) {
+      for (j = i + 1; j < n; j += 1) {
+        if (Math.abs(this.get(i, j)) >= max) {
+          max = Math.abs(this.get(i, j));
+          k = i;
+          l = j;
+        }
+      }
+    }
+
+    // Find c and s
+    let t: number;
+    if (Math.abs(this.get(k, l)) < Math.abs(this.get(l, l)) * 1e-36) {
+      t = this.get(k, l) / this.get(l, l);
+    } else {
+      const phi: number = this.get(l, l) / 2 * this.get(k, l);
+      t = 1 / (Math.abs(phi) + Math.sqrt(phi * phi + 1));
+    }
+
+    const c: number = 1 / Math.sqrt(t * t + 1);
+    const s: number = t * c;
+
+    const e: number = this.get(k, l);
+    this.set(k, l, 0);
+    this.set(k, k, this.get(k, k) - t * e);
+    this.set(l, l, this.get(l, l) + t * e);
+
+    // Rotate rows and columns k and l
+    for (i = 0; i < k; i += 1) {
+      rotate(this, c, s, i, k, i, l);
+    }
+
+    for (i = k + 1; i < l; i += 1) {
+      rotate(this, c, s, k, i, i, l);
+    }
+
+    for (i = l + 1; i < n; i += 1) {
+      rotate(this, c, s, k, i, l, i);
+    }
+
+    // Rotate eigenvectors
+    for (i = 0; i < n; i += 1) {
+      rotate(p, c, s, i, k, i, l);
+    }
+  } while (max >= 1e-9);
+
+  return [this.diagonal(), p];
 };
