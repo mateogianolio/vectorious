4e0ea16e7c26f0e4616fcc0720273515d18eb10b
Mateo Gianolio <gianoliomateo@gmail.com>
2021-04-17 13:48:44 +0200

    Simplify toArray

--- src/core/toArray.spec.ts
+++ src/core/toArray.spec.ts
@@ -1,22 +1,22 @@
-import { equals } from './equals';
+import { deepStrictEqual } from 'assert';
 import { array } from './array';
 import { toArray } from './toArray';
 
 describe('(v) toArray', () => {
   it('should work as expected', () => {
-    equals([], array().toArray());
-    equals([0, 1, 2, 3, 4, 5, 6, 7], array([0, 1, 2, 3, 4, 5, 6, 7]).toArray());
-    equals([[0, 1, 2, 3], [4, 5, 6, 7]], array([[0, 1, 2, 3], [4, 5, 6, 7]]).toArray());
-    equals([[[0, 1], [2, 3]], [[4, 5], [6, 7]]], array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]]).toArray());
-    equals([[0], [1], [2], [3], [4], [5], [6], [7]], array([[0], [1], [2], [3], [4], [5], [6], [7]]).toArray());
-    equals([[1, 4, 7], [2, 5, 8], [3, 6, 9]], array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).T.toArray());
+    deepStrictEqual([], array().toArray());
+    deepStrictEqual([0, 1, 2, 3, 4, 5, 6, 7], array([0, 1, 2, 3, 4, 5, 6, 7]).toArray());
+    deepStrictEqual([[0, 1, 2, 3], [4, 5, 6, 7]], array([[0, 1, 2, 3], [4, 5, 6, 7]]).toArray());
+    deepStrictEqual([[[0, 1], [2, 3]], [[4, 5], [6, 7]]], array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]]).toArray());
+    deepStrictEqual([[0], [1], [2], [3], [4], [5], [6], [7]], array([[0], [1], [2], [3], [4], [5], [6], [7]]).toArray());
+    deepStrictEqual([[1, 4, 7], [2, 5, 8], [3, 6, 9]], array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).T.toArray());
   });
 
   it('should work as the static equivalent', () => {
-    equals([], array().toArray());
-    equals([0, 1, 2, 3, 4, 5, 6, 7], toArray(array([0, 1, 2, 3, 4, 5, 6, 7])));
-    equals([[0, 1, 2, 3], [4, 5, 6, 7]], toArray(array([[0, 1, 2, 3], [4, 5, 6, 7]])));
-    equals(
+    deepStrictEqual([], array().toArray());
+    deepStrictEqual([0, 1, 2, 3, 4, 5, 6, 7], toArray(array([0, 1, 2, 3, 4, 5, 6, 7])));
+    deepStrictEqual([[0, 1, 2, 3], [4, 5, 6, 7]], toArray(array([[0, 1, 2, 3], [4, 5, 6, 7]])));
+    deepStrictEqual(
       [[[0, 1], [2, 3]], [[4, 5], [6, 7]]],
       toArray(array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]]))
     );
