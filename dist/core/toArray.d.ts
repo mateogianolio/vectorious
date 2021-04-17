a52ed8fe60b9ffd4356a29c9d0d1ca172e97ee91
Mateo Gianolio <gianoliomateo@gmail.com>
2021-04-17 15:00:08 +0200

    Generate dist

--- dist/core/toArray.d.ts
+++ dist/core/toArray.d.ts
@@ -11,15 +11,17 @@ import { NDArray } from './';
  *
  * toArray([1, 2, 3]); // => [1, 2, 3]
  */
-export declare const toArray: (x: NDArray | ArrayLike<any>) => number[];
+export declare const toArray: (x: NDArray | ArrayLike<any>) => any;
 /**
  * @function toArray
  * @memberof NDArray.prototype
  * @description Converts current vector into a JavaScript array.
+ * @param {Number} index
+ * @param {Number} dim
  * @returns {Array}
  * @example
  * import { array } from 'vectorious/core/array';
  *
  * array([1, 2, 3]).toArray(); // => [1, 2, 3]
  */
-export default function (this: NDArray): number[];
+export default function (this: NDArray, index?: number, dim?: number): any;
