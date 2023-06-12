cd2b8ff43bc112d495ddd80b58a3e3be1c2f3acc
Mateo Gianolio <gianoliomateo@gmail.com>
2023-06-12 23:31:20 +0200

    Improve test coverage (#240)
    * Improve test for multidimensional iterator
    
    * Improve tests for matrix multiply
    
    * Add blas.ts tests (except gemm)
    
    * Test unoptimized path for add()
    
    * Test unoptimized path for dot()
    
    * Test unoptimized path for inv()
    
    * Test unoptimized path for multiply()

--- src/core/dot.spec.ts
+++ src/core/dot.spec.ts
@@ -1,9 +1,46 @@
 import { strictEqual } from 'assert';
 
+import * as blas from '../blas';
 import { dot } from './dot';
 import { array } from './array';
 
 describe('(v) dot', () => {
+  it('should work as expected', () => {
+    const x = array([1, 2, 3]);
+    const y = array([4, 5, 6]);
+
+    strictEqual(32, x.dot(y));
+  });
+
+  it('should work with inc_x !== inc_y', () => {
+    const x = array([1, 2, 3]);
+    const y = array([1, 2, 3, 4, 5, 6], {
+      shape: [3],
+      strides: [2],
+    });
+
+    strictEqual(22, x.dot(y));
+  });
+
+  it('should work as the static equivalent', () => {
+    const x = array([1, 2, 3]);
+    const y = array([4, 5, 6]);
+
+    strictEqual(32, dot(x, y));
+  });
+});
+
+describe('(v) dot', () => {
+  beforeEach(() => {
+    jest.spyOn(blas, 'dot').mockImplementation(() => {
+      throw new Error();
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should work as expected', () => {
     const x = array([1, 2, 3]);
     const y = array([4, 5, 6]);
--- src/core/multiply.spec.ts
+++ src/core/multiply.spec.ts
@@ -1,5 +1,6 @@
 import { ok, throws } from 'assert';
 
+import * as blas from '../blas';
 import { equals } from './equals';
 import { multiply } from './multiply';
 import { array } from './array';
@@ -17,7 +18,7 @@ describe('(v) multiply', () => {
     }, Error);
   });
 
-  it('should work as expected', () => {
+  it('should multiply row and column matrices correctly', () => {
     const x = array([[1, 2]]);
     const y = array([[1], [2]]);
     const z = array([[5]]);
@@ -26,11 +27,35 @@ describe('(v) multiply', () => {
       [2, 4],
     ]);
 
-    ok(equals(z, x.copy().multiply(y)));
-    ok(equals(u, y.copy().multiply(x)));
+    ok(equals(z, x.multiply(y)));
+    ok(equals(u, y.multiply(x)));
   });
 
-  it('should work as expected', () => {
+  it('should rectangular matrices correctly', () => {
+    const x = array([
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ]);
+    const y = array([
+      [1, 2, 3],
+      [4, 5, 6],
+    ]);
+    const z = array([
+      [9, 12, 15],
+      [19, 26, 33],
+      [29, 40, 51],
+    ]);
+    const u = array([
+      [22, 28],
+      [49, 64],
+    ]);
+
+    ok(equals(z, x.multiply(y)));
+    ok(equals(u, y.multiply(x)));
+  });
+
+  it('should multiply a square matrix by itself as expected', () => {
     const x = array([
       [1, 2, 3],
       [4, 5, 6],
@@ -45,7 +70,7 @@ describe('(v) multiply', () => {
     ok(equals(y, x.multiply(x)));
   });
 
-  it('should work as expected', () => {
+  it('should multiply square matrices correctly', () => {
     const x = array([
       [0, 1, 0],
       [1, 0, 0],
@@ -69,6 +94,109 @@ describe('(v) multiply', () => {
     const x = array([[1], [2], [3]]);
     const y = array([[1, 1, 1]]);
 
-    ok(equals(x.copy().multiply(y), multiply(x, y)));
+    ok(equals(x.multiply(y), multiply(x, y)));
+  });
+});
+
+describe('(v) multiply (without gemm)', () => {
+  beforeEach(() => {
+    jest.spyOn(blas, 'gemm').mockImplementation(() => {
+      throw new Error();
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should throw error if sizes do not match', () => {
+    const x = array([
+      [1, 2],
+      [3, 4],
+    ]);
+    const y = array([[1, 2]]);
+
+    throws(() => {
+      x.multiply(y);
+    }, Error);
+  });
+
+  it('should multiply row and column matrices correctly', () => {
+    const x = array([[1, 2]]);
+    const y = array([[1], [2]]);
+    const z = array([[5]]);
+    const u = array([
+      [1, 2],
+      [2, 4],
+    ]);
+
+    ok(equals(z, x.multiply(y)));
+    ok(equals(u, y.multiply(x)));
+  });
+
+  it('should rectangular matrices correctly', () => {
+    const x = array([
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ]);
+    const y = array([
+      [1, 2, 3],
+      [4, 5, 6],
+    ]);
+    const z = array([
+      [9, 12, 15],
+      [19, 26, 33],
+      [29, 40, 51],
+    ]);
+    const u = array([
+      [22, 28],
+      [49, 64],
+    ]);
+
+    ok(equals(z, x.multiply(y)));
+    ok(equals(u, y.multiply(x)));
+  });
+
+  it('should multiply a square matrix by itself as expected', () => {
+    const x = array([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ]);
+    const y = array([
+      [30, 36, 42],
+      [66, 81, 96],
+      [102, 126, 150],
+    ]);
+
+    ok(equals(y, x.multiply(x)));
+  });
+
+  it('should multiply square matrices correctly', () => {
+    const x = array([
+      [0, 1, 0],
+      [1, 0, 0],
+      [0, 0, 1],
+    ]);
+    const y = array([
+      [1, 3, 5],
+      [2, 4, 7],
+      [1, 1, 0],
+    ]);
+    const z = array([
+      [2, 4, 7],
+      [1, 3, 5],
+      [1, 1, 0],
+    ]);
+
+    ok(equals(z, x.multiply(y)));
+  });
+
+  it('should work as the static equivalent', () => {
+    const x = array([[1], [2], [3]]);
+    const y = array([[1, 1, 1]]);
+
+    ok(equals(x.multiply(y), multiply(x, y)));
   });
 });
